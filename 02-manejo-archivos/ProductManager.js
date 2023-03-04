9d0b39b3e8a66d3375c850269b98a3137a8db892
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2023-03-04 13:14:32 -0300

    refactor: :recycle: use sync and async fs functionalities

--- 02-manejo-archivos/ProductManager.js
+++ 02-manejo-archivos/ProductManager.js
@@ -1,78 +1,97 @@
 const fs = require("fs");
 module.exports = class ProductManager {
-  #rowCount;
-  #path;
   constructor(path) {
-    this.#path = path;
-
+    this.path = path;
     (async () => {
-      const raw = await fs.promises.readFile(path, { encoding: "utf-8" });
-      const data = JSON.parse(raw);
-      const lastId = data[data.length - 1].id;
-      return lastId;
-    })().then(result => {
-      this.#rowCount = result;
-    }).catch(e => {
-      this.#rowCount = 0;
-    })
-
+      if(!fs.existsSync(path)) {
+        this.rowCount = 0;
+      } else {
+        const raw = await fs.promises.readFile(path, { encoding: "utf-8" });
+        const data = JSON.parse(raw);
+        this.rowCount = data[data.length - 1].id;
+      }
+    })()
   }
 
-  #getNextId() {
-    return this.#rowCount + 1;
+  getNextId() {
+    return this.rowCount + 1;
   }
 
-  #setNewId(callback) {
+  /**
+   * Funcion que permite setear un nuevo id en el roucount de la clase
+   * @param {function(rowCount)} callback Callback que recibe el contador de filas y permite manipular el nuevo valor del id
+   * @returns void
+   */
+  setNewId(callback) {
     if(!callback) {
-      this.#rowCount += 1;
+      this.rowCount += 1;
     } else {
-      this.#rowCount = callback(this.#rowCount);
+      this.rowCount = callback(this.rowCount);
     }
-    return this.#rowCount;
+    return this.rowCount;
   }
 
   async addProduct(productData) {
-    const newId = this.#getNextId()
-    const { title, description, price, thumbnail, code, stock } = productData;
-    if(!title || !description || !price || !thumbnail || !code || !stock) {
-      throw new Error('Invalid or missing properties on addProduct');
-    }
-    
-    const newProduct = { id: newId, title, description, price, thumbnail, code, stock };
-    let data;
     try {
-      const file = await fs.promises.readFile(this.#path, { encoding: "utf-8" });
-      data = JSON.parse(file);
-    }
-    catch(error) {
-      // Case when file does not exist.
-      await fs.promises.writeFile(this.#path, JSON.stringify([newProduct], null, "\t"));
-      this.#setNewId();
+      // Valido que los campos esten presentes
+      const { title, description, price, thumbnail, code, stock } = productData;
+      if(!title || !description || !price || !thumbnail || !code || !stock) {
+        throw new Error('Invalid or missing properties on addProduct');
+      }
+      // Voy creando el nuevo producto
+      const newId = this.getNextId()
+      const newProduct = { id: newId, title, description, price, thumbnail, code, stock };
+      
+      // Valido si existe el archivo y en caso de no existir guardo el producto como primer elemento del arreglo.
+      if(!fs.existsSync(this.path)) {
+        await fs.promises.writeFile(this.path, JSON.stringify([newProduct], null, "\t"));
+        this.setNewId();
+        return newProduct;
+      }
+      // Caso donde el archivo ya existe. Leo el archivo y lo parseo
+      const file = await fs.promises.readFile(this.path, { encoding: "utf-8" });
+      let data = JSON.parse(file);
+
+      // Verifico que el codigo no exista
+      const found = this.getProductByCode(code)
+      if(found) {
+        throw new Error(`The product with code: ${ code } is already exists`);
+      }
+      
+      // Guardo el producto en el archivo
+      const newProducts = [...data, newProduct];
+      await fs.promises.writeFile(this.path, JSON.stringify(newProducts, null, "\t"));
+      this.setNewId();
       return newProduct;
+    }catch(error) {
+      throw error;
     }
-    // Case when file exists
-    if(data.find(product => product.code === code)) {
-      throw new Error(`The product with code: ${ code } is already exists`);
-    }
-    const newProducts = [...data, newProduct];
-    await fs.promises.writeFile(this.#path, JSON.stringify(newProducts, null, "\t"));
-    this.#setNewId();
-    return newProduct;
+  }
+
+  async getProductByCode(code) {
+    if(!fs.existsSync(this.path)) return null
+
+    const file = await fs.promises.readFile(this.path, { encoding: "utf-8" });
+    let data = JSON.parse(file);
+    return data.find(product => product.code === code)
   }
 
   async getProducts() {
     try {
-      const data = await fs.promises.readFile(this.#path, { encoding: "utf-8" });
+      if(!fs.existsSync(this.path)) {
+        return []
+      }
+      const data = await fs.promises.readFile(this.path, { encoding: "utf-8" });
       return JSON.parse(data);
     }
     catch(error) {
-      return [];
+      throw error;
     }
   }
 
   async getProductById(productId) {
     try {
-      const response = await fs.promises.readFile(this.#path, { encoding: "utf-8" });
+      const response = await fs.promises.readFile(this.path, { encoding: "utf-8" });
       const data = JSON.parse(response);
       const productFound = data.find(product => product.id === productId);
       if(!productFound) {
@@ -87,7 +106,7 @@ module.exports = class ProductManager {
 
   async updateProductById(productId, changes) {
     try {
-      const response = await fs.promises.readFile(this.#path, { encoding: "utf-8" });
+      const response = await fs.promises.readFile(this.path, { encoding: "utf-8" });
       const data = JSON.parse(response);
       const productFound = data.find(product => product.id === productId);
       if(!productFound) {
@@ -100,7 +119,7 @@ module.exports = class ProductManager {
         }
         return product;
       })
-      await fs.promises.writeFile(this.#path, JSON.stringify(newData, null, "\t"));
+      await fs.promises.writeFile(this.path, JSON.stringify(newData, null, "\t"));
       return updated;
     }
     catch(error) {
@@ -110,14 +129,14 @@ module.exports = class ProductManager {
 
   async deleteProductById(productId) {
     try {
-      const response = await fs.promises.readFile(this.#path, { encoding: "utf-8" });
+      const response = await fs.promises.readFile(this.path, { encoding: "utf-8" });
       const data = JSON.parse(response);
       if(!data.find(product => product.id === productId)) {
         throw(new Error(`Product with id ${productId} does not exists`));
       }
       const newData = data.filter(product => product.id !== productId);
       console.log({productId, preDeleted: data, postDeleted: newData})
-      await fs.promises.writeFile(this.#path, JSON.stringify(newData, null, "\t"));
+      await fs.promises.writeFile(this.path, JSON.stringify(newData, null, "\t"));
     }
     catch(error) {
       throw error
